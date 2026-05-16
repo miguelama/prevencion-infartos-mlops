879187055b1ac1f135ee438d4de5286805e6f65c
miguelama <63486542+miguelama@users.noreply.github.com>
2026-05-16 13:29:03 -0600

    Create preprocessing.py

---
+++ data/src/preprocessing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def preprocess_data(input_path, output_path):
+    # 1. Cargar
+    df = pd.read_csv(input_path, sep=';')
+    
+    # 2. Limpiar nulos (ej. IMC)
+    df['IMC'] = df['IMC'].fillna(df['IMC'].median())
+    
+    # 3. Transformar categorías (One-Hot Encoding)
+    # Seleccionamos las columnas de texto para transformar
+    cols_to_encode = ['Genero', 'Tipo_trabajo', 'Zona_residencia', 'Flag_fumador', 'Estados_civil']
+    df_final = pd.get_dummies(df, columns=cols_to_encode, drop_first=True)
+    
+    # 4. Guardar
+    df_final.to_csv(output_path, index=False)
+    print(f"Datos procesados guardados en: {output_path}")
+
+if __name__ == "__main__":
+    preprocess_data('data/raw/Dataset_prevencion_infartos.csv', 'data/processed/data_clean.csv')
