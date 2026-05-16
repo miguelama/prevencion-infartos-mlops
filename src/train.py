3f83819a0e345bc1c970a73ad1d0182edec27640
miguelama <63486542+miguelama@users.noreply.github.com>
2026-05-16 16:21:44 -0600

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 import joblib
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def train_model(input_path, model_output_path):
     # 1. Cargar datos limpios
@@ -29,6 +31,7 @@ def train_model(input_path, model_output_path):
     
     umbral = 0.05
     nuevas_predicciones = (y_probs > umbral).astype(int)
+    guardar_matriz(y_test, nuevas_predicciones)
     
     reporte = classification_report(y_test, nuevas_predicciones)
     print("\n📊 Reporte de Evaluación (Umbral 0.2):")
@@ -44,7 +47,21 @@ def train_model(input_path, model_output_path):
     os.makedirs(os.path.dirname(model_output_path), exist_ok=True)
     joblib.dump(model, model_output_path)
     print(f"\n✅ Modelo guardado en: {model_output_path}")
-    
+
+def guardar_matriz(y_real, y_pred):
+    cm = confusion_matrix(y_real, y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', 
+                xticklabels=['Sano', 'Riesgo'], 
+                yticklabels=['Sano', 'Riesgo'])
+    plt.ylabel('Realidad')
+    plt.xlabel('Predicción')
+    plt.title(f'Matriz de Confusión (Umbral {umbral})')
+    plt.savefig('models/confusion_matrix.png') # Se guarda en la carpeta de artefactos
+    plt.close()
+
+
 
 if __name__ == "__main__":
     train_model('data/processed/data_clean.csv', 'models/heart_attack_model.pkl')
+  
