4e6da58f404bd32e27d3412512f53079841e5c1c
miguelama <63486542+miguelama@users.noreply.github.com>
2026-05-16 14:03:48 -0600

    Update preprocessing.py

--- data/src/preprocessing.py
+++ data/src/preprocessing.py
@@ -11,20 +11,23 @@ def validate_data(df):
     print("✅ Validación exitosa: Los datos son consistentes.")
 
 def preprocess_data(input_path, output_path):
-    # 1. Cargar
+    # 1. Cargar (Asegúrate de que el nombre coincida con tu carpeta data/raw)
     df = pd.read_csv(input_path, sep=';')
     
-    # 2. Limpiar nulos (ej. IMC)
+    # 2. Limpiar nulos
     df['IMC'] = df['IMC'].fillna(df['IMC'].median())
     
-    # 3. Transformar categorías (One-Hot Encoding)
-    # Seleccionamos las columnas de texto para transformar
+    # 3. Transformar categorías
     cols_to_encode = ['Genero', 'Tipo_trabajo', 'Zona_residencia', 'Flag_fumador', 'Estados_civil']
     df_final = pd.get_dummies(df, columns=cols_to_encode, drop_first=True)
     
+    # --- PASO DE VALIDACIÓN ---
+    # Llamamos a la función antes de guardar
+    validate_data(df_final) 
+    
     # 4. Guardar
     df_final.to_csv(output_path, index=False)
-    print(f"Datos procesados guardados en: {output_path}")
+    print(f"✅ Datos validados y guardados en: {output_path}")
 
 if __name__ == "__main__":
     preprocess_data('data/raw/Dataset_prevencion_infartos.csv', 'data/processed/data_clean.csv')
